91e975d80a1b5dab46906d8f47bed228b9d92179
Huillca Reynaldo <lymreynaldo@icloud.com>
2024-07-25 21:56:52 -0300

    feat: add listener

---
+++ readme.md
@@ -0,0 +1,19 @@
+## Payload in queue
+```json
+{
+    "codigoPedido": 1001,
+    "codigoCliente": 1,
+    "itens": [
+        {
+            "produto": "lápis",
+            "quantidade": 100,
+            "preco": 1.10
+        },
+        {
+            "produto": "caderno",
+            "quantidade": 10,
+            "preco": 1.00
+        }
+    ]
+}
+```
---
+++ src/main/java/uk/layme/btgpactual/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,21 @@
+package uk.layme.btgpactual.orderms.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import uk.layme.btgpactual.orderms.listener.dto.OrderCreatedEvent;
+
+import static uk.layme.btgpactual.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message) {
+        logger.info("Received message: {}", message.getPayload());
+    }
+}
